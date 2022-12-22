e902de121dc5471e242a6ede47a25df5d6941ead
Ian McBee <imcbee@terpmail.umd.edu>
2022-12-22 17:21:21 -0600

    new project for hibernate, created test class

---
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,30 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		
+		try {
+			
+			System.out.println("Conneting to Database: " + jdbcUrl);
+			
+			Connection myConnection = DriverManager.getConnection(jdbcUrl, user, password);
+			
+			System.out.println("Connection Successful!!!");
+			
+		}catch(Exception exc) {
+			
+			exc.printStackTrace();
+			
+		}
+		
+	}
+
+}
