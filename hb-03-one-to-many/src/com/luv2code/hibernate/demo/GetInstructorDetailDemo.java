539cbb61aa7cf467a874128e5743dbffbd28f27a
Ian McBee <imcbee@terpmail.umd.edu>
2023-01-16 11:57:51 -0500

    first commit

---
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,60 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start the transaction
+			session.beginTransaction(); //this will save the details object because cascade all
+			
+			// get the instructor detail object
+			int getId = 2999;
+				//having a bad number will give a null
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, getId);
+			
+			// print the instructor detail object
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			// print the associated instructor
+			System.out.println("the associated Instructor" + tempInstructorDetail.getInstructor());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!!!");
+			
+		} catch(Exception exc) {
+			
+			exc.printStackTrace();
+			
+		} finally {
+			
+			// handle connection leak issue
+			session.close();
+			
+			factory.close();
+			
+		}
+
+	}
+
+}
