f9c1e25019dbcf7b96f3628b97b6bb5d36b09c68
Ian McBee <mcbee_ian@bah.com>
2023-09-20 16:53:58 -0400

    working through springboot overview and core

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+  // define private fields for the dependency
+  private Coach myCoach;
+
+  @Autowired
+  public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
+    myCoach = theCoach;
+  }
+  @GetMapping("/")
+  public String getWorkingApp() {
+    return "Working app";
+  }
+
+  @GetMapping("/dailyworkout")
+  public String getDailyWorkout() {
+    return myCoach.getDailyWorkout();
+  }
+}
