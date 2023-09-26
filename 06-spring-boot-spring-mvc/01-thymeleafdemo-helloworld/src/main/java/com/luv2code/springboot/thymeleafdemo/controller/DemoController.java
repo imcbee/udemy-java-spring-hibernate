48d09a8535c1c1b7d57df7c5cbfe2dac3535df9e
Ian McBee <mcbee_ian@bah.com>
2023-09-26 15:08:06 -0400

    thymeleaf and mvc practice

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+  // create a mapping for "/hello"
+
+  @GetMapping("/hello")
+  public String sayHello(Model theModel) {
+
+    theModel.addAttribute("theDate", new java.util.Date());
+
+    return "helloworld"; // this matches the html file in templates
+  }
+}
