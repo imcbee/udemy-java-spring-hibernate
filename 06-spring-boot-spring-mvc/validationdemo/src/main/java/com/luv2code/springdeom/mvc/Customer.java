44efa67a66024d02050245446f8f4396eeedf3b6
Ian McBee <mcbee_ian@bah.com>
2023-09-28 12:15:18 -0400

    custom validation message

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdeom/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdeom/mvc/Customer.java
@@ -1,8 +1,7 @@
 package com.luv2code.springdeom.mvc;
 
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +18,13 @@ public class Customer {
   @NotNull(message = "is required")
   @Size(min=1, message = "is required")
   private String lastName = "";
+
+  @Min(value = 0, message = "must be greater than or equal to zero")
+  @Max(value = 10, message = "must be less than or equal to 10")
+  @NotNull(message = "is required")
+  private Integer freePasses; //? the trimmer will handle this wrapper class
+
+  @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+  private String postalCode;
 
 }
