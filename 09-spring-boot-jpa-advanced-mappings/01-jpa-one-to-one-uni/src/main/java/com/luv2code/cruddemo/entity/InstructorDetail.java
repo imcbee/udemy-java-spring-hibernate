6cf41ee0a969d465e1ec39e791f7be00d2199ddb
Ian McBee <mcbee_ian@bah.com>
2023-10-20 14:51:18 -0400

    update

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,39 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="instructor_detail")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class InstructorDetail {
+
+  // annotate the class as an entity and map to db table
+
+  // define the fields
+  // annotate the fields with db column names
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "youtube_channel")
+  private String youtubeChannel;
+
+  @Column(name = "hobby")
+  private String hobby;
+
+  // create constructors
+  public InstructorDetail(String youtubeChannel, String hobby) {
+    this.youtubeChannel = youtubeChannel;
+    this.hobby = hobby;
+  }
+
+  // generate getter/setter methods
+
+  // generate toString() method
+}
